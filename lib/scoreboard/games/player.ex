b21926d51566528d5e002d20ff164ca5b10e74c4
Aaron Votre <avotre422@gmail.com>
2018-07-09 17:49:11 -0400

    Remove many_to_many macro

--- lib/scoreboard/games/player.ex
+++ lib/scoreboard/games/player.ex
@@ -1,15 +1,13 @@
 defmodule Scoreboard.Games.Player do
   use Ecto.Schema
   import Ecto.Changeset
-  alias Scoreboard.Games.{Game, Score}
+  alias Scoreboard.Games.Score
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "players" do
     field(:name, :string)
-
     has_many(:scores, Score)
-    many_to_many(:games, Game, join_through: Score)
 
     timestamps()
   end
